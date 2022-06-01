cb5eea404ece112f75e15453e5e91bbbc0b865f7
Aidan945 <zarskia9@gmail.com>
2022-06-01 12:53:17 -0500

    bmi

---
+++ Age_calc.py
@@ -0,0 +1,9 @@
+import os
+os.system('cls')
+
+
+birth_year = float(input("input your year of birth "))
+current_year = float(input("input the current year "))
+
+age = current_year - birth_year
+print("your age is",age)
