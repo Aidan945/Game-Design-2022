fc27773bea2e24f82937edccf8c5ff7562bddbfb
Aidan945 <zarskia9@gmail.com>
2022-06-04 09:16:16 -0500

    commit

--- BMI_calc.py
+++ BMI_calc.py
@@ -1,3 +1,15 @@
+# Aidan Zarski
+
+# Pseudocode
+# start
+# input the height(m)
+# input the weight(kg)
+# do weight/(height*height) to find BMI
+# display BMI
+# if BMI is less than 14 say "your underweight"
+# elif bmi is greater than 40 say "your overweight"
+# else say "your normal"
+
 import os
 os.system('cls')
 
